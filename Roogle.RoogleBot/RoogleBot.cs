0f82ea4df92c14c5731d73dd4a26c103e98b457e
Caitlin Wilks <cat@cat.bio>
2021-08-09 17:38:51 +0100

    add placeholder RoogleBot project

---
+++ Roogle.RoogleBot/RoogleBot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Roogle.RoogleBot
+{
+  /// <summary>
+  /// The roogle bot, posts roogle updates on talkhaus
+  /// </summary>
+  public class RoogleBot
+  {
+    /// <summary>
+    /// Entrypoint
+    /// </summary>
+    static void Main()
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
